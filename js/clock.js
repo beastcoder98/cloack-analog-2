8bb14f97c4f2afb8086ed20be1e7c3bfd0add80d
Charif Abdallah <charifabdallah@iut-dhaka.edu>
2021-08-15 19:21:56 +0600

    Watch design commit

---
+++ js/clock.js
@@ -0,0 +1,26 @@
+// we are going to grab the reference of .second-hand,.hour-hand,.min-hand by using querySelector()
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    // new Date() creates an instance of the Date class from whiich we can get all
+    // kinds of things like current date,hours,minutes,seconds etc.
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate();
